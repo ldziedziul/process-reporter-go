1330ac7950d0046d8f504e265d5c9334e18efe9b
Łukasz Dziedziul <l.dziedziul@gmail.com>
2025-05-16 13:44:15 +0200

    Add integration tests

--- main.go
+++ main.go
@@ -3,35 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"os"
 )
 
-func main() {
-	format := flag.String("format", "json", "Output format: json or csv")
-	output := flag.String("output", "", "Optional output file name. If not set, output goes to stdout")
-	flag.Parse()
+func run(args []string, sysOut io.Writer, sysErrOut io.Writer) error {
+	fs := flag.NewFlagSet("process-reporter", flag.ContinueOnError)
+	fs.SetOutput(sysErrOut) // Flag errors go to errOut
+
+	format := fs.String("format", "json", "Output format: json or csv")
+	output := fs.String("output", "", "Optional output file name. If not set, output goes to stdout")
+
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
 
 	formatter, err := getFormatter(*format)
 	if err != nil {
-		log.Fatalf("Unsupported format: %v", err)
+		return fmt.Errorf("unsupported format: %v", err)
 	}
 
 	provider := &SystemProcessProvider{}
-	var out *os.File
-	if *output == "" {
-		out = os.Stdout
-	} else {
-		out, err = os.Create(*output)
+
+	var out = sysOut
+	var file *os.File
+	if *output != "" {
+		file, err = os.Create(*output)
 		if err != nil {
-			log.Fatalf("Failed to create output file: %v", err)
+			return fmt.Errorf("failed to create output file: %v", err)
 		}
-		defer func(out *os.File) {
-			err := out.Close()
-			if err != nil {
-				log.Fatalf("Failed to close output file: %v", err)
-			}
-		}(out)
+		defer file.Close()
+		out = file
 	}
 
 	app := App{
@@ -41,10 +43,19 @@ func main() {
 	}
 
 	if err := app.Run(); err != nil {
-		log.Fatalf("Failed to run application: %v", err)
+		return fmt.Errorf("failed to run application: %v", err)
 	}
 
 	if *output != "" {
-		fmt.Printf("Report saved successfully in %s format to %s\n", *format, *output)
+		fmt.Fprintf(sysOut, "Report saved successfully in %s format to %s\n", *format, *output)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(os.Args[1:], os.Stdout, os.Stderr); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
